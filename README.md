4ef203233980c0eb078ad8c88865781f18400377
ckroeger <ckroeger.hh@gmail.com>
2023-07-02 09:16:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# wiremock-playground
+
+A playground for wiremock exploration.
+
+## Links
+* [WireMock at Github](https://github.com/wiremock)
+* [WireMock - flexible, open source API mocking | WireMock](https://wiremock.org/)
---
+++ src/main/java/com/github/ckroeger/wiremockplayground/WiremockPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.github.ckroeger.wiremockplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiremockPlaygroundApplication.class, args);
+    }
+
+}
